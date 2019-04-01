90dd82a88c21879f5f276f76138b9f3beb144892
eddy-reynoso <reynosoeddy@yahoo.com>
2019-04-01 16:49:46 -0400

    Added DB and Middleware in Server.JS

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -1,20 +1,24 @@
-db = new Mongo().getDB('issuetracker');
+db = new Mongo().getDB('fitnesstracker');
 
-db.issues.remove({});
+db.meals.remove({});
 
-db.issues.insert([
+db.meals.insert([
   {
-    status: 'Open', owner: 'Ravan',
-    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-    title: 'Error in console when clicking Add',
+    foodName: "Omelette",
+    mealType: "Breakfast",
+    numberOfServings: 1,
+    calories: 350,
+    fat: 35,
+    carbohydrates: 12
   },
   {
-    status: 'Assigned', owner: 'Eddie',
-    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel',
+    foodName: "Grilled Chicken",
+    mealType: "Lunch",
+    numberOfServings: 2,
+    calories: 160,
+    fat: 5,
+    carbohydrates: 0
   },
 ]);
 
-db.issues.createIndex({ status: 1 });
-db.issues.createIndex({ owner: 1 });
-db.issues.createIndex({ created: 1 });
+
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const MongoClient = require('mongodb');
+const bodyParser = require('body-parser'); 
 
 const app = express();
 
 app.use(express.static('static'));
-
+app.use(bodyParser.json());
 app.listen(3000, function () {
     console.log('App started on port 3000');
 });
