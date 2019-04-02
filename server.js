951d9b47637da1e4e007ed92b13d5955b4162b18
Jack Broomer <jbroomer@umass.edu>
2019-04-01 23:22:02 -0400

    "Fixed Bad Merge"

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const MongoClient = require('mongodb');
 const bodyParser = require('body-parser'); 
 
 const app = express();
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+
 
 app.use(bodyParser.json());
 app.use(express.static('static'));
-<<<<<<< HEAD
 
 const bodystats = [
     {
@@ -34,9 +32,6 @@ const bodystats = [
   res.json(newIssue);
 });
 
-=======
-app.use(bodyParser.json());
->>>>>>> 90dd82a88c21879f5f276f76138b9f3beb144892
 app.listen(3000, function () {
     console.log('App started on port 3000');
 });
