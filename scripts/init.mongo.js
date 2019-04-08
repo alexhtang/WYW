bf5576f2640a63f0b817d6c386eb5069ba570dae
Jack Broomer <jbroomer@umass.edu>
2019-04-08 16:06:13 -0400

    "Database works for App.jsx"

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -2,6 +2,14 @@ db = new Mongo().getDB('fitnesstracker');
 
 db.userBodyStats.remove({});
 
+db.userBodyStats.insert([
+    {
+      height: "0",
+      weight: "0",
+      age: "0",
+      gender: "N/A",
+    },]);
 
 
+db.meals.remove({});
 
--- server.js
+++ server.js
@@ -8,25 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('static'));
 
-const bodystats = [
-  {
-    height: "0",
-    weight: "0",
-    age: "0",
-    gender: "N/A"
-  }
-];
-
-  function updateStat(a){
-    bodystats[0] = a;
-  }
 
   
 
   app.get('/api/data', (req, res) => {
     db.collection('userBodyStats').find().toArray().then(bodystats => {
-      const metadata = { metadata: 'metadata' };
-      res.json({ _metadata: metadata, records: bodystats })
+      res.json({ records: bodystats })
     }).catch(error => {
       console.log(error);
       res.status(500).json({ message: `Internal Server Error: ${error}` });
@@ -34,22 +21,18 @@ const bodystats = [
   });
 
   app.post('/api/data', (req, res) => {
-  const newStats = req.body;
-  newStats.created = new Date();
-
-  updateStat(newStats);
-  res.json(newStats);
-
-
-db.collection('userBodyStats').insertOne(newStats).then(result =>
-    db.collection('userBodyStats').find({ _id: result.insertedId }).limit(1).next()
-  ).then(bodystats => {
-    res.json(bodystats);
-  }).catch(error => {
-    console.log(error);
-    res.status(500).json({ message: `Internal Server Error: ${error}` });
+    const newIssue = req.body;
+    db.collection('userBodyStats').deleteOne();
+    
+    db.collection('userBodyStats').insertOne(newIssue).then(result =>
+      db.collection('userBodyStats').find({ _id: result.insertedId }).limit(1).next()
+    ).then(newIssue => {
+      res.json(newIssue);
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({ message: `Internal Server Error: ${error}` });
+    });
   });
-});
 
 let db;
 MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
